7217484f73df45a15714c337ef782e4cf903bfc5
Sanskriti Vidushi <vidushisanskriti@gmail.com>
2024-07-04 21:19:08 +0530

    Arrays Page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -2,38 +2,41 @@ import React from "react";
 // import '/index.css'
 // import { Link } from "react-router-dom";
 
-const LandingPage = ({ handleSort }) => {
+const LandingPage = ({ handleSort, generateRandomArray, setSpeed, speed }) => {
     return (
       <div>
         <div className="background"></div>
-    <div className="content-wrapper">
-           <div className="content-box">
            <div class="container">
         <div class="header"> 
-            <span class="name"><b>ARRAYS</b></span>
+            <span class="name"><b>ARRAY SORTING ALGORITHMS</b></span>
         </div>
-        <div class="choose"><h2>Choose Sorting Algorithm</h2></div>
+        <div class="choose"><h2>Choose Algorithm</h2></div>
         <div class="buttons_container">
-            <button id="randomize_arr_btn">GENERATE RANDOM ARRAY</button>
+            <button id="randomize_arr_btn" onClick={generateRandomArray}>GENERATE RANDOM ARRAY</button>
             <div class="algo-buttons">
-                <button id="bubble_button" onClick={() => handleSort('bubble')}>Bubble Sort</button>
-                <button id="select_button" >SELECTION SORT</button>
-                <button id="merge_button" >MERGE SORT</button>
+                <button id="bubble_button" onClick={() => handleSort('bubble')}>BUBBLE SORT</button>
+                <button id="select_button" onClick={() => handleSort('selection')}>SELECTION SORT</button>
+                <button id="merge_button"  onClick={() => handleSort('merge')}>MERGE SORT</button>
                 <button id="quick_button" >QUICK SORT</button>
                 </div>
                 </div>
             <div class="speed-buttons">
                 <div class="text">SLOWER</div>
-                <input type="range" id="speed-slider" min="1" max="1000" defaultValue="100" />
+                <input
+                type="range"
+                id="speed-slider"
+                min="10"
+                max="1000"
+                value={1050-speed}
+                onChange={(e) => setSpeed(1050-Number(e.target.value))}
+              />
+                {/* <input type="range" id="speed-slider" min="1" max="1000" defaultValue="100" /> */}
                 <div class="text">FASTER</div>
             </div>
         <div class="bars_container" id="bars_container">
         </div>
-    </div>
+       </div>
           </div>
-        </div>
-      </div>
     );
   }
-  
   export default LandingPage;
--- frontend/src/pages/arraysPage.js
+++ frontend/src/pages/arraysPage.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import 'frontend/src/index.css';
+import React from "react";
+// import '/index.css'
+// import { Link } from "react-router-dom";
 
-const arraysPage = () => {
-  return (
-    <div>
-      <div className="background"></div>
-      <div className="content-wrapper">
-        <div className="content-box">
-          <div className="container">
-            <div className="header">
-              <span className="name"><b>SORTING ALGORITHMS</b></span>
-            </div>
-            <div className="choose"><h2>Choose Sorting Algorithm</h2></div>
-            <div className="buttons_container">
-              <button id="randomize_arr_btn">GENERATE RANDOM ARRAY</button>
-              <div className="algo-buttons">
-                <button id="bubble_button">BUBBLE SORT</button>
-                <button id="select_button">SELECTION SORT</button>
-                <button id="merge_button">MERGE SORT</button>
-                <button id="quick_button">QUICK SORT</button>
-              </div>
-              <div className="speed-buttons">
-                <div className="text">SLOWER</div>
-                <input type="range" id="speed-slider" min="1" max="1000" defaultValue="100" />
-                <div className="text">FASTER</div>
-              </div>
-            </div>
-            <div className="bars_container" id="bars_container"></div>
-            <div>
-              <Link to="/">Back to Home</Link>
-            </div>
-          </div>
+const ArraysPage = ({ handleSort, generateRandomArray, setSpeed, speed }) => {
+    return (
+      <div>
+        <div className="background"></div>
+           <div class="container">
+        <div class="header"> 
+            <span class="name"><h1><b>ARRAY SORTING ALGORITHMS</b></h1></span>
         </div>
-      </div>
-    </div>
-  );
-}
-
-export default arraysPage;
+        <div class="choose"><h2>Choose an algorithm to visualise its sorting</h2></div>
+        <div class="buttons_container">
+            <button id="randomize_arr_btn" onClick={generateRandomArray}>GENERATE RANDOM ARRAY</button>
+            <div class="algo-buttons">
+                <button id="bubble_button" onClick={() => handleSort('bubble')}>BUBBLE SORT</button>
+                <button id="select_button" onClick={() => handleSort('selection')}>SELECTION SORT</button>
+                <button id="merge_button"  onClick={() => handleSort('merge')}>MERGE SORT</button>
+                <button id="quick_button"  onClick={() => handleSort('quick')}>QUICK SORT</button>
+                </div>
+                </div>
+            <div class="speed-buttons">
+                <div class="text">SLOWER</div>
+                <input
+                type="range"
+                id="speed-slider"
+                min="10"
+                max="1000"
+                value={1050-speed}
+                onChange={(e) => setSpeed(1050-Number(e.target.value))}
+              />
+                {/* <input type="range" id="speed-slider" min="1" max="1000" defaultValue="100" /> */}
+                <div class="text">FASTER</div>
+            </div>
+        <div class="bars_container" id="bars_container">
+        </div>
+       </div>
+          </div>
+    );
+  }
+  export default ArraysPage;
