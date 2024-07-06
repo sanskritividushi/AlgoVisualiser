a6eff22ee0e96f6b1c931f61d7bba48bbcf15d2e
Sukriti Punj <sukritipunj@gmail.com>
2024-07-06 23:49:31 +0530

    navbar update

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+// import '/index.css'
+
+import { Link } from "react-router-dom";
+
+
+const LandingPage = () => {
+    return (
+      <div>
+        <div className="background"></div>
+      <div className="navbar">
+        <Link to="./arraysPage">Arrays</Link>
+        <Link to="./treesPage">Trees</Link>
+        <Link to="./graphs">Graphs</Link>
+        <Link to="/LandingPage">HOME</Link>
+        <Link to="/backtracking">Backtracking</Link>
+      </div>
+      <div className="content-wrapper">
+        <div className="content-box">
+          <h1>ALGOVISUALISER</h1>
+          <p align="left">Arrays, sorting, and trees are fundamental concepts that play a crucial role in organizing and 
+          manipulating data efficiently. Arrays provide a simple way to store and access multiple elements in a contiguous 
+          memory block. Sorting algorithms help us order data in a meaningful way, 
+          while trees allow for efficient searching and hierarchical organization.</p>
+          
+
+        </div>
+      </div>
+      </div>
+    );
+  }
+  export default LandingPage;
+
+  /* 
+  
+  */
---
+++ frontend/src/pages/backtracking.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';  // Adjust path as necessary
+
+const BacktrackingPage = () => {
+  return (
+    <div className="content-wrapper">
+      <div className="content-box">
+        <h1>Backtracking</h1>
+        
+      </div>
+    </div>
+  );
+}
+
+export default BacktrackingPage;
