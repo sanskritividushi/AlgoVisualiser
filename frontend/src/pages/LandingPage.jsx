6593986858260aa887a556e586e87f2af9679385
Sanskriti Vidushi <vidushisanskriti@gmail.com>
2024-08-03 15:22:13 +0530

    Landing Page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import './ds.css';
 import { Link } from "react-router-dom";
 
-
 const LandingPage = () => {
     return (
       <div>
@@ -17,19 +16,14 @@ const LandingPage = () => {
       </div>
       <div className="content-wrapper">
         <div className="content-box">
-          <h1>ALGOVISUALISER</h1>
-          <p align="center">Arrays, sorting, and trees are fundamental concepts that play a crucial role in organizing and 
-          manipulating data efficiently. Arrays provide a simple way to store and access multiple elements in a contiguous 
-          memory block. Sorting algorithms help us order data in a meaningful way, 
-          while trees allow for efficient searching and hierarchical organization.</p>
-
+          <h1 padding="20px">ALGOVISUALISER</h1>
+            <p class="intro" align="center">Data structures are the building blocks of techonology with respect to software development.
+              The utilisation of any data structure carries a common goal, simplifying the mechanics of our project and making our work more effortless.
+              Data structures like Arrays, Trees, Graphs are fundamental concepts that play a crucial role in accomplishing these goals.
+              To understand the working of these, no tool can be more helpful than visualisation. </p>
         </div>
       </div>
       </div>
     );
   }
   export default LandingPage;
-
-  /* 
-  
-  */
